cf615d14b91ef8df213122138a8e0864fdc70986
Stefano Calzoni <stefanoclz@hotmail.it>
2022-03-10 17:01:18 +0100

    aggiunto calcolo e stampa a schermo esercizio 3

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -3,5 +3,21 @@ console.log('JS OK')
 // Chiedi un numero di 4 cifre all’utente e calcola la somma
 // di tutte le cifre che compongono il numero.
 
-let sum = 0;
+let result = document.getElementById('result');
 
+const calcola = document.getElementById('calcola');
+
+
+
+calcola.addEventListener('click', function () {
+    let sum = 0;
+    let number = parseInt(prompt('Inserisci un numero a 4 cifre'));
+    while (number) {
+        sum += number % 10;
+        number = Math.floor(number / 10);
+    }
+
+    console.log(sum);
+
+    result.innerHTML = `Il risultato dei numeri inseriti è: ${sum}`;
+})
